4dd4c77c03349e4d52a9c1b7d88df421dfef4004
Jeff Weisberg <jaw@tcp4me.com>
2014-02-16 00:17:05 -0500

    fix

--- lib/Talmud/Passwd.pm
+++ lib/Talmud/Passwd.pm
@@ -4,24 +4,48 @@
 # Author: Jeff Weisberg <jaw @ tcp4me.com>
 # Date: 2003-Oct-20 13:06 (EDT)
 # Function: 
-#
-# $Id: Passwd.pm,v 1.2 2010/01/16 03:21:48 jaw Exp $
+
 
 package Talmud::Passwd;
+use MIME::Base64;
+use Digest::SHA1 qw(sha1 sha1_base64);
 
-use Digest::MD5 ('md5_base64');
+my $ITER = 13;
 
 sub encrypt {
     my $plain = shift;
-    
-    md5_base64($plain);
+    my $salt  = shift;
+
+    my $ver;
+    my $iter;
+    if( $salt ){
+        ($ver, $iter, $salt, undef) = split /\$/, $salt;
+    }else{
+        my $text = sha1_base64($$ . time() . $ITER . rand());
+        $text =~ tr|+/||d;
+        $salt = substr($text, 0, 8);
+        $ver  = 'T1';
+        $iter = $ITER;
+    }
+
+    my $hash = $plain;
+    if( $ver eq 'T1' ){
+        $hash = sha1($salt . $hash . $salt) for (1 .. (1<<$ITER));
+    }else{
+        # ...
+    }
+    $hash = encode_base64($hash);
+    $hash =~ tr|+/=||d;
+
+    return join('$', $ver, $iter, $salt, $hash);
 }
 
 sub verify {
     my $plain = shift;
     my $crypt = shift;
 
-    encrypt($plain) eq $crypt;
+    encrypt($plain, $crypt) eq $crypt;
 }
+
 
 1;
